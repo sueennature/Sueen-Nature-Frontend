c9fcc7b97b1be48b2c1ea429c0ab860efe563e78
Developer-CDAZZ <161746443+Developer-CDAZZ@users.noreply.github.com>
2024-06-28 14:59:28 +0530

    Update contactBody.vue
    Correct phone number

--- components/contactBody.vue
+++ components/contactBody.vue
@@ -188,7 +188,7 @@
           <a
             href="tel:+94715840000"
             class="text-stone-700 hover:underline text-base"
-            >+(94) 71 58 4000
+            >+(94) 71 58 40000
           </a>
         </div>
       </div>
@@ -267,7 +267,7 @@
           <a
             href="https://wa.me/+94715840000"
             class="text-stone-700 hover:underline text-base"
-            >+(94) 71 58 4000</a
+            >+(94) 71 58 40000</a
           >
         </div>
       </div>
